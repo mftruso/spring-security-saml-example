d79a569f896432b4e6a5f1040abd9cfc1c17e9a1
Mike Truso <mike@agileorbit.com>
2019-11-11 13:35:05 -0600

    initial commit

---
+++ src/main/java/com/miketruso/springsecurity/saml/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.miketruso.springsecurity.saml.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/miketruso/springsecurity/saml/demo/config/CustomLoginSuccessHandler.java
@@ -0,0 +1,17 @@
+package com.miketruso.springsecurity.saml.demo.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomLoginSuccessHandler  extends SavedRequestAwareAuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        System.out.println("Authentication Success!");
+        authentication.getAuthorities().forEach(authority -> System.out.println(authority.getAuthority()));
+    }
+}
